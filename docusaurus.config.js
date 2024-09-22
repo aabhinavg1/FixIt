493463125826f277fbe288fa586a081580f6631b
aabhinavg <tiwariabhinavak@gmail.com>
2024-09-22 14:09:19 +0530

    modifying the name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production URL of your site here
-  url: 'https://https://gandiv.netlify.app/',
+  url: 'https://gandiv.netlify.app/',
   // Set the base pathname under which your site is served
   baseUrl: '/',
 
