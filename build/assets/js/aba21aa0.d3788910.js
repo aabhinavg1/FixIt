9801d1dcc943370aa6ce0f55c1c6b79f88efdc3f
aabhinavg1 <tiwariabhinavak@gmail.com>
2024-10-07 18:37:18 +0530

    fix seo related things

--- build/assets/js/aba21aa0.d3788910.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfixit=self.webpackChunkfixit||[]).push([[5742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- build/assets/js/acecf23e.44113c77.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfixit=self.webpackChunkfixit||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
