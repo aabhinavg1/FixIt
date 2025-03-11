922ac3cd9eb8a4be4f9365f95f280929fceb3efc
aabhinavg1 <tiwariabhinavak@gmail.com>
2025-03-11 12:17:28 +0530

    added analytic

--- static/js/google-tag-manager.js
+++ static/js/google-tag-manager.js
@@ -3,9 +3,14 @@
     w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
     var f = d.getElementsByTagName(s)[0],
       j = d.createElement(s),
-      dl = l != 'dataLayer' ? '&l=' + l : '';
+      dl = l !== 'dataLayer' ? '&l=' + l : '';
     j.async = true;
     j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
     f.parentNode.insertBefore(j, f);
   })(window, document, 'script', 'dataLayer', 'G-4PW5BRLTHD');
+  
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', 'G-4PW5BRLTHD', { 'send_page_view': false });
   
