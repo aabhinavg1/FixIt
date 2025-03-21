d2d5b27cf2de9783ae3180357318e161a235e364
aabhinavg1 <tiwariabhinavak@gmail.com>
2025-03-21 09:42:31 +0530

    updated the tag manager

--- static/js/google-analytics.js
+++ static/js/google-analytics.js
@@ -1,4 +1,24 @@
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
+
+// Load Google Tag Manager with default consent settings (opt-out until user grants consent)
+gtag('consent', 'default', {
+  'ad_storage': 'denied', // Deny ad storage (e.g., cookies for ads)
+  'analytics_storage': 'denied', // Deny analytics storage (e.g., GA cookies)
+  'personalization_storage': 'denied', // Deny personalized content storage
+  'functionality_storage': 'granted', // Allow necessary cookies
+  'security_storage': 'granted' // Allow security-related cookies
+});
+
+// Initialize GA4
 gtag('js', new Date());
 gtag('config', 'G-CJDGBRKJ5W');
+
+// When user accepts cookies, update consent
+function grantConsent() {
+  gtag('consent', 'update', {
+    'ad_storage': 'granted',
+    'analytics_storage': 'granted',
+    'personalization_storage': 'granted'
+  });
+}
--- static/js/google-tag-manager.js
+++ static/js/google-tag-manager.js
@@ -1,16 +1,38 @@
 (function(w, d, s, l, i) {
-    w[l] = w[l] || [];
-    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-    var f = d.getElementsByTagName(s)[0],
+  w[l] = w[l] || [];
+  w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+  var f = d.getElementsByTagName(s)[0],
       j = d.createElement(s),
       dl = l !== 'dataLayer' ? '&l=' + l : '';
-    j.async = true;
-    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-    f.parentNode.insertBefore(j, f);
-  })(window, document, 'script', 'dataLayer', 'G-4PW5BRLTHD');
-  
-  window.dataLayer = window.dataLayer || [];
-  function gtag() { dataLayer.push(arguments); }
-  gtag('js', new Date());
-  gtag('config', 'G-4PW5BRLTHD', { 'send_page_view': false });
-  
+  j.async = true;
+  j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+  f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'G-4PW5BRLTHD');
+
+// Ensure dataLayer exists
+window.dataLayer = window.dataLayer || [];
+
+// Define gtag function
+function gtag() { dataLayer.push(arguments); }
+
+// Set default consent mode (deny analytics & ads until user accepts)
+gtag('consent', 'default', {
+  'ad_storage': 'denied', 
+  'analytics_storage': 'denied', 
+  'personalization_storage': 'denied', 
+  'functionality_storage': 'granted', 
+  'security_storage': 'granted'
+});
+
+// Initialize Google Analytics (but prevent auto page view tracking)
+gtag('js', new Date());
+gtag('config', 'G-4PW5BRLTHD', { 'send_page_view': false });
+
+// Function to update consent when user accepts
+function grantConsent() {
+  gtag('consent', 'update', {
+      'ad_storage': 'granted',
+      'analytics_storage': 'granted',
+      'personalization_storage': 'granted'
+  });
+}
