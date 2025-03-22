b29bfc918abc87b920845c37e52d2ce0a9685a9c
aabhinavg1 <tiwariabhinavak@gmail.com>
2025-03-22 10:58:05 +0530

    added the mathematical fondaton

--- static/js/contest.js
+++ static/js/contest.js
@@ -27,7 +27,10 @@ function updateConsent(consentGiven) {
   if (consentGiven) {
     // ✅ Load Google Analytics if consent is granted
     gtag('js', new Date());
-    gtag('config', 'G-CJDGBRKJ5W', { 'send_page_view': false }); // Replace with your GA4 ID
+    
+    // ✅ Configure multiple tracking IDs
+    gtag('config', 'G-CJDGBRKJ5W', { 'send_page_view': false });
+    gtag('config', 'G-4PW5BRLTHD', { 'send_page_view': false });
   }
 }
 
@@ -50,14 +53,17 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// ✅ Load GTM only after setting consent mode
-(function(w, d, s, l, i) {
+// ✅ Load both GTM containers
+(function(w, d, s, l, ids) {
   w[l] = w[l] || [];
   w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-  var f = d.getElementsByTagName(s)[0],
-      j = d.createElement(s),
-      dl = l !== 'dataLayer' ? '&l=' + l : '';
-  j.async = true;
-  j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-  f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'G-CJDGBRKJ5W');
+
+  ids.forEach(id => {
+    var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l !== 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + id + dl;
+    f.parentNode.insertBefore(j, f);
+  });
+})(window, document, 'script', 'dataLayer', ['G-CJDGBRKJ5W', 'G-4PW5BRLTHD']);
