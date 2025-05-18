70c72c8f68bc01485ba88a6c842809d9436aa13b
aabhinavg1 <tiwariabhinavak@gmail.com>
2025-05-18 09:16:53 +0530

    updated docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -133,18 +133,20 @@ const config = {
     {
       src: 'https://www.googletagmanager.com/gtm.js?id=GTM-N8G7MKF9',
       async: true,
+      defer: true,
     },
     {
       src: "https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js",
       async: true,
+      defer: true,
       "custom-element": "amp-auto-ads",
     },
-    { src: '/js/contest.js', async: true },
-    { src: '/js/amp-auto-ads.js', async: true },
-    { src: '/js/google-tag-manager.js', async: true },
-    { src: '/js/google-analytics.js', async: true },
-    { src: '/js/structured.js', async: true },
-    { src: '/js/llvm.js', async: true },
+    { src: '/js/contest.js', async: true, defer: true},
+    { src: '/js/amp-auto-ads.js', async: true, defer: true },
+    { src: '/js/google-tag-manager.js', async: true, defer: true },
+    { src: '/js/google-analytics.js', async: true, defer: true },
+    { src: '/js/structured.js', async: true, defer: true },
+    { src: '/js/llvm.js', async: true, defer: true  },
   ],
 
   customFields: {
